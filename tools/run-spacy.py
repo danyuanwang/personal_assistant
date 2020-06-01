985b720ed6e2a8db71497c71c02d2e6a5043d579
zhigangw <zhigangw@hotmail.com>
2020-05-31 18:07:11 -0700

    update run-spacy.py

--- tools/run-spacy.py
+++ tools/run-spacy.py
@@ -4,10 +4,13 @@ from spacy import displacy
 spacy_model = "en_core_web_md"
 print("\nload spacy model:", spacy_model)
 nlp = spacy.load(spacy_model)
-
+visualize_dep = False
 while(True):
+    option_dic = {
+            'q':'quit', 
+            'v':'visualize dependency parse'
+            } 
     sample_sentences = [
-            'quit', 
             'who is the smartest person in the world?',
             'where is the statue of liberty?',
             'how many have died due to covid-19 in the world',
@@ -17,20 +20,27 @@ while(True):
     print("\n")
     for i in range(0 , len(sample_sentences)):
         print(i," - ", sample_sentences[i])
+    print('\n')
+    for option in option_dic:
+        print(option,' - ', option_dic[option])
 
-    text = input('choose an option or type your text[1]:')
+    text = input('choose an option or type your text[0]:')
+    if(len(text) < 1): 
+       text = '0'
+
     if(text.isnumeric()):
         inx = int(text)
-        if(inx < len(sample_sentences) and inx > 0):
-            text = sample_sentences[inx]
+        if(inx >= len(sample_sentences) or inx < 0):
+            continue
         else:
-            text = '0'
+            text = sample_sentences[inx]
 
-    if(len(text) < 1): 
-       text = sample_sentences[1]
-
-    if(text == '0' or text == 'q'):
+    if(text == 'q' or text == 'quit'):
         break
+    if(text == 'v'):
+       visualize_dep = not visualize_dep 
+       print('visualize dependency parse is', visualize_dep)
+       continue
 
     doc = nlp(text)
     token_features = ['token.text', 'token.lemma_', 'token.pos_', 'token.tag_', 'token.dep_',
@@ -56,5 +66,5 @@ while(True):
     print(ent_feature_format.format(*ent_features))
     for ent in doc.ents:
        print(ent_feature_format.format(ent.text, ent.start_char, ent.end_char, ent.label_))
-
-#    displacy.serve(doc, style="dep")
+    if(visualize_dep):
+       displacy.serve(doc, style="dep")
